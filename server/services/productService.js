80bd1d69ebbd889d72b8a1aaea48b364f1f67e29
SteveO <orregoe@gmail.com>
2023-02-05 22:55:00 -0300

    Buscardor de productos like con RegExp

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,14 +1,17 @@
 const Product = require('../models/productModel')
 
 const productService = {
-    findAllProducts: async (filter, page, limit) => {
+    findAllProducts: async (query , page, limit) => {
         // return await Product.find(filter).populate('categories')
         // const produ = await Product.find(filter).populate('categories')
         const options = {
             page,
-            limit
+            limit,
         };
-        return await Product.paginate({ filter }, options)
+
+        console.log(query);
+
+        return await Product.paginate(query, options)
     },
     saveProduct: async (product) => {
         const newProduct = new Product(product)
